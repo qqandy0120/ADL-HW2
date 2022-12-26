05a25f472cd4b6ee39c097ee51432848c5e86570
Andy <qqandy0120@github.com>
2022-12-26 10:59:47 +0800

    two link

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
-gdown 15uKNS19ol5231hpkqmECV5KwXmxlesq4
-unzip ckpt_mac.zip
+gdown 15uKNS19ol5231hpkqmECV5KwXmxlesq4 || gdown 1EhM6lsNkPKGAS8DgAoa40wE-1E6Id9Jz
+unzip ckpt_mac.zip || unzip ckpt_mac2.zip
